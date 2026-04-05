5cc0fd9bd59461d9bda2352a4dfbb5f7d198365c
SHAHABAS P <148606053+shahabasparamban@users.noreply.github.com>
2026-04-05 15:24:24 +0400

    Updated UI and fixed bugs

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -9,27 +9,27 @@ const Skills = () => {
     
       <section id ="skills">
         <span className="skillTittle">What I do</span>
-        <span className="skillDesign">I am a skilled and passionate web designer</span>
+        <span className="skillDesign">I am an entry-level developer who designs user-friendly interfaces and builds responsive websites and simple mobile app designs.</span>
         <div className="skillBars">
             <div className="skillBar">
                 <img src={img10} alt="" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>UI/UX Design</h2>
-                    <p>This is a demo</p>
+                    <p>I design intuitive and user-friendly digital experiences by combining clean UI design with thoughtful UX principles.</p>
                 </div>
             </div>
              <div className="skillBar">
                 <img src={img12} alt="" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>Web Design</h2>
-                    <p>This is a demo text</p>
+                    <p>I design and develop responsive, modern websites with a focus on clean design and user experience.</p>
                 </div>
             </div>
              <div className="skillBar">
                 <img src={img13} alt="" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>App Design</h2>
-                    <p>This is dempo</p>
+                    <p>I design intuitive and user-friendly mobile applications with a focus on simplicity and usability.</p>
                 </div>
             </div>
         </div>
