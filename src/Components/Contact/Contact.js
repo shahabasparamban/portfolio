25ba7c33acb3984c996aa0f8d6a76c39b929feef
SHAHABAS P <148606053+shahabasparamban@users.noreply.github.com>
2026-04-06 08:41:13 +0400

    Updated UI and fixed bugs

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,6 +10,27 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
+    const name = form.current.name.value.trim();
+    const email = form.current.name.value.trim();
+    const message = form.current.name.value.trim();
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!name) {
+      alert('Please enter your name');
+      return;
+    }
+    if (!message) {
+      alert('Please enter your message');
+      return;
+    }
+
+    if (!emailPattern.test(email)) {
+      alert('Please enter a valid email address');
+      return;
+    }
+
+
     emailjs
       .sendForm('service_gob64fr', 'template_wucdp6c', form.current, {
         publicKey: 'BwR1v3TV3e8Nv30Ld',
@@ -27,21 +48,21 @@ const Contact = () => {
   };
   return (
     <section id='contactPage'>
-        <div id='contact'>
-            <h1 className="contactPageTittle">Contact Me</h1>
-            <span className="contactDesc">Please fill out the form to discuss any work opportunities</span>
-            <form className="contactForm" ref={form} onSubmit={sendEmail}>
-            <input type="text" className="name" placeholder='Your Name' name='name'/>
-            <input type="text" className="email" placeholder='Your Email' name='email'/>
-            <textarea className="msg" name="message" rows={5} placeholder='message'></textarea>
-            <button type="submit" value="Send" className="SubmitBtn">Submit</button>
-            <div className="links">
-                <img src={youtube} alt="Youtube" className="link" />
-                <img src={fbicon} alt="Facebook" className="link" />
-                <img src={linkedin} alt="Linkedin" className="link" />
-            </div>
-            </form>
-        </div>
+      <div id='contact'>
+        <h1 className="contactPageTittle">Contact Me</h1>
+        <span className="contactDesc">Please fill out the form to discuss any work opportunities</span>
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+          <input type="text" className="name" placeholder='Your Name' name='name' />
+          <input type="text" className="email" placeholder='Your Email' name='email' />
+          <textarea className="msg" name="message" rows={5} placeholder='message'></textarea>
+          <button type="submit" value="Send" className="SubmitBtn">Submit</button>
+          <div className="links">
+            <img src={youtube} alt="Youtube" className="link" />
+            <img src={fbicon} alt="Facebook" className="link" />
+            <img src={linkedin} alt="Linkedin" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import './Resume.css';
+import React from "react";
+import "./Resume.css";
 
 const Resume = () => {
-
-const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = '/cv.pdf'; // from public folder
-    link.download = 'MyCV.pdf';
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/cv.pdf"; // from public folder
+    link.download = "MyCV.pdf";
     link.click();
   };
 
   return (
-   <section id='resume'>
-    <div className='cv'>
-        <p className='text'>Here is my cv and you can Download it!!</p>
-    <button className="download" onClick={handleDownload}>
+    <section id="resume">
+      <div className="cv">
+        <p className="text">Here is my cv and you can Download it!!</p>
+        <button className="download" onClick={handleDownload}>
           <span>Download CV</span>
         </button>
-   
-   </div>
-   </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
 export default Resume;
